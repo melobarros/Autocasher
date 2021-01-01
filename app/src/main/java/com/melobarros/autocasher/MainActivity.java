e76bee8806405d4ea7e4a69f37f65ae9cb66f93c
Amauri Barros <melo.barros@pucpr.edu.br>
2021-01-01 07:06:22 -0300

    Project init

---
+++ app/src/main/java/com/melobarros/autocasher/MainActivity.java
@@ -0,0 +1,14 @@
+package com.melobarros.autocasher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
