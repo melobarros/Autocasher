a59f5e3ef02d0a423cf6787aa4d0aff2ce8a0d46
Amauri Barros <melo.barros@pucpr.edu.br>
2021-01-04 07:34:26 -0300

    Fixed bug and set up bottom nav options

--- app/src/main/java/com/melobarros/autocasher/MainActivity.java
+++ app/src/main/java/com/melobarros/autocasher/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Configurar bottom navigation view
+        configuraBottomNavigationView();
+    }
+
+    private void configuraBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
     }
 }
