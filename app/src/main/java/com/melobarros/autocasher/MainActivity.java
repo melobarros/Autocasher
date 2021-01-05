86399b7520250ad1943b8c41cb70e5097c653c63
Amauri Barros <melo.barros@pucpr.edu.br>
2021-01-05 08:21:20 -0300

    Created fragments and fixed initial button

--- app/src/main/java/com/melobarros/autocasher/MainActivity.java
+++ app/src/main/java/com/melobarros/autocasher/MainActivity.java
@@ -1,10 +1,21 @@
 package com.melobarros.autocasher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.melobarros.autocasher.fragment.AbastecimentoFragment;
+import com.melobarros.autocasher.fragment.GastoFragment;
+import com.melobarros.autocasher.fragment.HistoricoFragment;
+import com.melobarros.autocasher.fragment.LembreteFragment;
+import com.melobarros.autocasher.fragment.ManutencaoFragment;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,16 +25,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Configurar bottom navigation view
+        // Configurar bottom navigation view
         configuraBottomNavigationView();
+
+        // Start Historico Fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.viewPager, new HistoricoFragment()).commit();
     }
 
     private void configuraBottomNavigationView(){
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
 
         bottomNavigationViewEx.enableAnimation(true);
-        //bottomNavigationViewEx.enableItemShiftingMode(true);
-        //bottomNavigationViewEx.enableShiftingMode(false);
-        //bottomNavigationViewEx.setTextVisibility(true);
+
+        habilitarNavegacao(bottomNavigationViewEx);
+
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+    }
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch(menuItem.getItemId()){
+                    case R.id.ic_historico :
+                        fragmentTransaction.replace(R.id.viewPager, new HistoricoFragment()).commit();
+                        return true;
+                    case R.id.ic_abastecimento :
+                        fragmentTransaction.replace(R.id.viewPager, new AbastecimentoFragment()).commit();
+                        return true;
+                    case R.id.ic_lembrete :
+                        fragmentTransaction.replace(R.id.viewPager, new LembreteFragment()).commit();
+                        return true;
+                    case R.id.ic_gasto :
+                        fragmentTransaction.replace(R.id.viewPager, new GastoFragment()).commit();
+                        return true;
+                    case R.id.ic_manutencao :
+                        fragmentTransaction.replace(R.id.viewPager, new ManutencaoFragment()).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/melobarros/autocasher/fragment/GastoFragment.java
@@ -0,0 +1,32 @@
+package com.melobarros.autocasher.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.melobarros.autocasher.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GastoFragment extends Fragment {
+
+
+    public GastoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gasto, container, false);
+    }
+
+}
