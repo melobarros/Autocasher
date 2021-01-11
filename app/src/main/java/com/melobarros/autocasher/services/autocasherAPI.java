02a07d4619d0b34c55cbb23662f6603f90acbdc9
Amauri Barros <melo.barros@pucpr.edu.br>
2021-01-11 07:47:09 -0300

    Created autocasherAPI inteface

---
+++ app/src/main/java/com/melobarros/autocasher/services/autocasherAPI.java
@@ -0,0 +1,107 @@
+package com.melobarros.autocasher.services;
+
+import com.melobarros.autocasher.model.Abastecimento;
+import com.melobarros.autocasher.model.Gasto;
+import com.melobarros.autocasher.model.Lembrete;
+import com.melobarros.autocasher.model.Manutencao;
+import com.melobarros.autocasher.model.Registro;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface autocasherAPI {
+
+    public static final String BASE_URL = "https://autocasher.herokuapp.com/api";
+
+    @GET("/registros")
+    Call<List<Registro>> getRegistros();
+
+    @GET("/registro/{id}")
+    Call<Registro> getRegistroById(@Path("id") int id);
+
+    @POST("/registro")
+    Call<String> insertRegistro(@Body Registro registro);
+
+    @PUT("/registro")
+    Call<String> updateRegistro(@Body Registro registro);
+
+    @DELETE("/registro")
+    Call<String> deleteRegistro(@Body Registro registro);
+
+    //------------------------------------------------------
+
+    @GET("/registro/gastos")
+    Call<List<Gasto>> getGastos();
+
+    @GET("/registro/gasto/{id}")
+    Call<Gasto> getGastoById(@Path("id") int id);
+
+    @POST("/registro/gasto")
+    Call<String> insertGasto(@Body Gasto gasto);
+
+    @PUT("/registro/gasto")
+    Call<String> updateGasto(@Body Gasto gasto);
+
+    @DELETE("/registro/gasto")
+    Call<String> deleteGasto(@Body Gasto gasto);
+
+
+    //------------------------------------------------------
+
+    @GET("/registro/abastecimentos")
+    Call<List<Abastecimento>> getAbastecimentos();
+
+    @GET("/registro/abastecimento/{id}")
+    Call<Abastecimento> getAbastecimentoById(@Path("id") int id);
+
+    @POST("/registro/abastecimento")
+    Call<String> insertAbastecimento(@Body Abastecimento abastecimento);
+
+    @PUT("/registro/abastecimento")
+    Call<String> updateAbastecimento(@Body Abastecimento abastecimento);
+
+    @DELETE("/registro/abastecimento")
+    Call<String> deleteAbastecimento(@Body Abastecimento abastecimento);
+
+    //------------------------------------------------------
+
+    @GET("/registro/lembretes")
+    Call<List<Lembrete>> getLembretes();
+
+    @GET("/registro/lembrete/{id}")
+    Call<Lembrete> getLembreteById(@Path("id") int id);
+
+    @POST("/registro/lembrete")
+    Call<String> insertLembrete(@Body Lembrete lembrete);
+
+    @PUT("/registro/lembrete")
+    Call<String> updateLembrete(@Body Lembrete lembrete);
+
+    @DELETE("/registro/lembrete")
+    Call<String> deleteLembrete(@Body Lembrete lembrete);
+
+    //------------------------------------------------------
+
+    @GET("/registro/manutencoes")
+    Call<List<Manutencao>> getManutencoes();
+
+    @GET("/registro/manutencao/{id}")
+    Call<Manutencao> getManutencaoById(@Path("id") int id);
+
+    @POST("/registro/manutencao")
+    Call<String> insertManutencao(@Body Manutencao manutencao);
+
+    @PUT("/registro/manutencao")
+    Call<String> updateManutencao(@Body Manutencao manutencao);
+
+    @DELETE("/registro/manutencao")
+    Call<String> deleteManutencao(@Body Manutencao manutencao);
+
+}
