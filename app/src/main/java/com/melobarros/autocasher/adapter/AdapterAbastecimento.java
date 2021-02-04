4a1a6e61bf00cabf29d919bda65226c62f8fc8d5
Amauri Barros <melo.barros@pucpr.edu.br>
2021-02-03 23:14:44 -0300

    Todo Lembrete

--- app/src/main/java/com/melobarros/autocasher/adapter/AdapterAbastecimento.java
+++ app/src/main/java/com/melobarros/autocasher/adapter/AdapterAbastecimento.java
@@ -70,7 +70,7 @@ public class AdapterAbastecimento extends RecyclerView.Adapter<AdapterAbastecime
         holder.subItem.setVisibility(abastecimento.isExpanded() ? View.VISIBLE : View.GONE);
         holder.tipoRegistro.setText(abastecimento.getTipo());
         holder.valorTotal.setText("R$ " + String.format("%.2f", abastecimento.getLitros() * abastecimento.getPrecoLitro()));
-        // to do essa semana
+        // to do lembrete
 
         holder.btn_delete.setOnClickListener(new View.OnClickListener() {
             @Override
