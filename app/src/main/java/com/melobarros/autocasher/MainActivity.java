65c7001fb1a38da754556059c8f36ad835b2a548
Amauri Barros <melo.barros@pucpr.edu.br>
2021-02-21 21:21:20 -0300

    Updating again

--- app/src/main/java/com/melobarros/autocasher/MainActivity.java
+++ app/src/main/java/com/melobarros/autocasher/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         - TO DO
             - Melhorias:
                 - Geral
-                    - Toolbar com botao para analyticsss
+                    - Toolbar com botao para analytics // VAMO LOGO
                     - Arrumar casos de uso em que usuario preenche campo com valor invalido (vazio, texto, numero)
                     - Opcional: circulo em volta dos icones
                 - Abastecimento
