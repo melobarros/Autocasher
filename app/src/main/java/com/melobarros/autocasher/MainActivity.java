9165118a1d5ac4d3d21d70d8698e51391e39d492
Amauri Barros <melo.barros@pucpr.edu.br>
2021-03-13 12:25:47 -0300

    Added requirement

--- app/src/main/java/com/melobarros/autocasher/MainActivity.java
+++ app/src/main/java/com/melobarros/autocasher/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         - TO DO
             - Melhorias:
                 - Geral
+                    - Swipe left e right pra trocar fragments
                     - Filtrar por data e ordenar por valor e data
                     - Implementar tela de Estatisticas
                     - Arrumar casos de uso em que usuario preenche campo com valor invalido (vazio, texto, numero)
