bd2c4e7c938859a982c6f2278fc9b8a624509f4e
Amauri Barros <melo.barros@pucpr.edu.br>
2021-03-16 08:09:49 -0300

    UI UX improvs

--- app/src/main/java/com/melobarros/autocasher/fragment/HistoricoFragment.java
+++ app/src/main/java/com/melobarros/autocasher/fragment/HistoricoFragment.java
@@ -3,19 +3,25 @@ package com.melobarros.autocasher.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.melobarros.autocasher.R;
 
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HistoricoFragment extends Fragment {
-
+    private static final String TAG = "HistoricoFragment";
+    Toolbar toolbar;
 
     public HistoricoFragment() {
         // Required empty public constructor
@@ -26,7 +32,21 @@ public class HistoricoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historico, container, false);
+        Log.d(TAG, "onCreateView: started.");
+        View view = inflater.inflate(R.layout.fragment_historico, container, false);
+
+        toolbar = view.findViewById(R.id.Estatisticas_toolbar);
+        initToolbar();
+
+        return view;
+    }
+
+    public void initToolbar(){
+        toolbar.setTitle("");
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_clear_black_24dp);
     }
 
 }
